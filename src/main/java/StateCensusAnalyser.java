55c996f1468ab02fc7ffa2f67e663702eb9e580d
rohini <patilrohini43@gmail.com>
2020-02-01 13:19:40 +0530

    Refactored:code for taking count of enteries in csv file

--- src/main/java/StateCensusAnalyser.java
+++ src/main/java/StateCensusAnalyser.java
@@ -12,13 +12,7 @@ public class StateCensusAnalyser {
         try {
             Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
             Iterator<IndiaCensusCSV> censusCSVIterator = this.getCSVFilterIterator(reader,IndiaCensusCSV.class);
-            int namOfEateries = 0;
-            while (censusCSVIterator.hasNext()) {
-                namOfEateries++;
-                IndiaCensusCSV censusData = censusCSVIterator.next();
-                System.out.println(censusData);
-            }
-            return namOfEateries;
+            return this.getCountCSVFile(censusCSVIterator);
         }
         catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
@@ -28,24 +22,28 @@ public class StateCensusAnalyser {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.HEADERNOTFOUND);
         }
-
     }
 
     public int loadIndiaStateCensusData(String csvFilePath) throws CensusAnalyserException {
         try {
             Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
             Iterator<CsvState> censusCSVIterator = this.getCSVFilterIterator(reader,CsvState.class);
-            int namOfEateries = 0;
-            while (censusCSVIterator.hasNext()) {
-                namOfEateries++;
-                CsvState censusData = censusCSVIterator.next();
-                System.out.println(censusData);
-            }
-            return namOfEateries;
+            return this.getCountCSVFile(censusCSVIterator);
         } catch (IOException e) {
             throw new CensusAnalyserException(e.getMessage(),
                     CensusAnalyserException.ExceptionType.TYPE_NOTFOUND);
         }
+    }
+
+    public <T> int getCountCSVFile(Iterator<T> censusCSVIterator)
+    {
+        int namOfEateries = 0;
+        while (censusCSVIterator.hasNext()) {
+            namOfEateries++;
+            T censusData = censusCSVIterator.next();
+            System.out.println(censusData);
+        }
+        return namOfEateries;
     }
 
     public <T> Iterator<T> getCSVFilterIterator(Reader reader, Class<T> csvStateClass)
